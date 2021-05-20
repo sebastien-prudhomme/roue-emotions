09ac1bbb40f644367942ca76f30e64117cf2e400
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2021-05-20 17:48:48 +0200

    fix: use new Twitter account

--- src/components/CarouselSlideEditor.vue
+++ src/components/CarouselSlideEditor.vue
@@ -1,7 +1,7 @@
 <template>
   <q-carousel-slide class="column items-center justify-center" :name="name">
     <div class="text-center">Sébastien Prud'homme</div>
-    <q-btn class="icon-half text-half" flat icon="fab fa-twitter" label="@sebasprud" no-caps rounded @click="open" />
+    <q-btn class="icon-half text-half" flat icon="fab fa-twitter" label="@CowboySysop" no-caps rounded @click="open" />
   </q-carousel-slide>
 </template>
 
@@ -18,7 +18,7 @@ export default {
   },
   methods: {
     open: function () {
-      openURL('https://twitter.com/sebasprud')
+      openURL('https://twitter.com/CowboySysop')
     }
   }
 }
