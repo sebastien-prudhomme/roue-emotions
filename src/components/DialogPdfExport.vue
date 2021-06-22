6f27844319681449a9db0bf244e41200d656a644
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2021-06-22 22:33:26 +0200

    feat: add mounted wheel animation

--- src/components/DialogPdfExport.vue
+++ src/components/DialogPdfExport.vue
@@ -1,6 +1,14 @@
 <template>
   <q-dialog class="dialog-export" persistent ref="dialog" @hide="hideDialog">
     <q-card class="q-dialog-plugin">
+      <q-card-section class="card-section-wheel">
+        <div class="wheel">
+          <div class="wheel__layer wheel__layer--1"></div>
+          <div class="wheel__layer wheel__layer--2"></div>
+          <div class="wheel__layer wheel__layer--3"></div>
+          <div class="wheel__layer wheel__layer--4"></div>
+        </div>
+      </q-card-section>
       <q-card-section>{{ $t('file_name') }}</q-card-section>
       <q-card-section>
         <q-input dense outlined suffix=".pdf" v-model="fileName" />
@@ -63,3 +71,89 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+@function ratio($amount)
+  @return calc(#{$amount} * min(2vw, 1vh))
+
+.card-section-wheel
+  display: flex
+  overflow: hidden
+  align-items: center
+  justify-content: center
+  height: ratio(30)
+
+.wheel
+  width: ratio(1.7 * 21)
+  height: ratio(1.7 * 29.7)
+  transform: rotateX(60deg) rotateZ(45deg)
+  transform-style: preserve-3d
+
+.wheel__layer
+  position: absolute
+  width: 100%
+  height: 100%
+  animation-duration: 6s
+  animation-iteration-count: infinite
+  background-size: cover
+  &--1
+    animation-name: wheel__layer--1
+    background-image: url('~assets/pdf-export-1.png')
+  &--2
+    animation-name: wheel__layer--2
+    background-image: url('~assets/pdf-export-2.png')
+  &--3
+    animation-name: wheel__layer--3
+    background-image: url('~assets/pdf-export-3.png')
+  &--4
+    animation-name: wheel__layer--4
+    background-image: url('~assets/pdf-export-4.png')
+
+@keyframes wheel__layer--1
+  0%
+    transform: rotateZ(0)
+  75%
+    transform: rotateZ(0)
+  95%
+    transform: rotateZ(45deg)
+  100%
+    transform: rotateZ(45deg)
+
+@keyframes wheel__layer--2
+  0%
+    transform: rotateZ(0) translateZ(ratio(3))
+  5%
+    transform: translateZ(ratio(3))
+  25%
+    transform: translateZ(0)
+  55%
+    transform: rotateZ(0)
+  75%
+    transform: rotateZ(-30deg)
+  100%
+    transform: rotateZ(-30deg) translateZ(0)
+
+@keyframes wheel__layer--3
+  0%
+    transform: rotateZ(0) translateZ(ratio(6))
+  10%
+    transform: translateZ(ratio(6))
+  30%
+    transform: translateZ(0)
+  35%
+    transform: rotateZ(0)
+  55%
+    transform: rotateZ(72deg)
+  100%
+    transform: rotateZ(72deg) translateZ(0)
+
+@keyframes wheel__layer--4
+  0%
+    transform: translateZ(ratio(9))
+  15%
+    transform: translateZ(ratio(9))
+  35%
+    transform: translateZ(0)
+  100%
+    transform: translateZ(0)
+</style>
